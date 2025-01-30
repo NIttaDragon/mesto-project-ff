1ec242abcaa73654844b180cc93c2307e8b9fed4
uspem <uspemskaya@gmail.com>
2025-01-30 20:34:04 +0300

    iter1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,37 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+function dellCard(element) {
+  element.remove()
+}
+
+function addCard(name, img/*, delliCard*/){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = img;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => dellCard(cardElement));
+  const placesList = document.querySelector('.places__list');
+  placesList.append(cardElement);
+}
+
+let i = 0;
+
+document.querySelector('.profile__add-button').addEventListener('click', function(event) {
+  if (i > 5) {
+    i = 0;
+  }
+  addCard(initialCards[i].name, initialCards[i].link/*, dellCard()*/);
+  i++;
+} )
+
+
+initialCards.forEach(function(elem) {
+  addCard(elem.name, elem.link/*, dellCard()*/);
+})
+
+
+
