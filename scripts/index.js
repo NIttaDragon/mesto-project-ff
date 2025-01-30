cca07d9aa7fdf847d0d862b57872a98d45a9beab
uspem <uspemskaya@gmail.com>
2025-01-31 00:23:07 +0300

    iter2

--- scripts/index.js
+++ scripts/index.js
@@ -8,37 +8,29 @@
 
 // @todo: Вывести карточки на страницу
 
+const placesList = document.querySelector('.places__list');
 
-
-function dellCard(element) {
+//функция удаления карточки
+function deleteCard(element) {
   element.remove()
 }
 
-function addCard(name, img/*, delliCard*/){
+//функция создания карточки
+function createCard(name, img){
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__image').src = img;
   cardElement.querySelector('.card__image').alt = name;
   cardElement.querySelector('.card__title').textContent = name;
-
-  cardElement.querySelector('.card__delete-button').addEventListener('click', () => dellCard(cardElement));
-  const placesList = document.querySelector('.places__list');
-  placesList.append(cardElement);
+  cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
+  
+  return cardElement;
 }
 
-let i = 0;
-
-document.querySelector('.profile__add-button').addEventListener('click', function(event) {
-  if (i > 5) {
-    i = 0;
-  }
-  addCard(initialCards[i].name, initialCards[i].link/*, dellCard()*/);
-  i++;
-} )
-
-
+//создание и вставка начальных карточек на экран
 initialCards.forEach(function(elem) {
-  addCard(elem.name, elem.link/*, dellCard()*/);
+  const cardElement = createCard(elem.name, elem.link);
+  placesList.append(cardElement);
 })
 
 
